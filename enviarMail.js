39a2d383ee80a731bf2326a5c57fc0d095864395
AgusRomero1404 <romeroagustin1404@gmail.com>
2024-02-27 15:07:11 -0300

    Modificacion del destinatario del mail a enviar.

--- enviarMail.js
+++ enviarMail.js
@@ -12,9 +12,9 @@ enviarMail = async (correoUsuario) => {
 
   const message = {
     from: "enviodemailpruebatecnicacober@gmail.com",
-    to: "romeroagustin1404@gmail.com",
-    subject: "Nuevo usuario agregado a la lista de espera",
-    text: `El correo electronico: ${correoUsuario} se a sumado a nuestra lista de espera`,
+    to: "admin@grupocober.online",
+    subject: "Nueva suscripción",
+    text: `El correo electronico: ${correoUsuario} se ha suscrito con exito `,
   };
   const transport = nodemailer.createTransport(config);
   const info = await transport.sendMail(message);
